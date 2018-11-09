f73914c3deefd5afbf8108b53a9c3d9608426e41
denizhfu <44021523+denizhfu@users.noreply.github.com>
2018-11-09 01:29:08 +0100

    Aufgabe_4_TO_DO_1

---
+++ C-Sharp4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace C_Sharp4
+{
+    class TestClass
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 0; i < 5; i++)
+            {
+                RondomName();
+            }
+        }
+        public static Random rand = new Random(DateTime.Now.Second);
+        public static void RondomName()
+        {
+            string[] firstNames = new string[] { "Anna", "Ben", "Chris", "Dani", "Emi" };
+            string[] lastNames = new string[] { "Failenschmid", "Google", "Haus", "Igel", "Jeder" };
+            string userName;
+            int age = rand.Next(17, 25);
+
+
+            var FirstName = firstNames[rand.Next(0, firstNames.Length)];
+            var LastName = lastNames[rand.Next(0, lastNames.Length)];
+            if (age >= 20)
+            {
+                userName = $"{FirstName} {LastName} {age}";
+                Console.WriteLine(userName);
+            }
+            else
+            {
+                Console.WriteLine(age + " ist zu jung!");
+            }
+        }
+    }
+}
+
